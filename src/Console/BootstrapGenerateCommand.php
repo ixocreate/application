83e84ab2e5b7d828728ce5b090d066d7dabf9f95
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-19 07:03:24 +0100

    bootstrap file generator

---
+++ src/Console/BootstrapGenerateCommand.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace KiwiSuite\Application\Console;
+
+use KiwiSuite\Application\ApplicationConfig;
+use KiwiSuite\ApplicationConsole\Command\CommandInterface;
+use KiwiSuite\Contract\Application\BootstrapItemInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class BootstrapGenerateCommand extends Command implements CommandInterface
+{
+    /**
+     * @var ApplicationConfig
+     */
+    private $applicationConfig;
+
+    private $template = <<<'EOD'
+<?php
+namespace App;
+/** @var %s %s */
+
+EOD;
+
+    /**
+     * BootstrapListCommand constructor.
+     */
+    public function __construct(ApplicationConfig $applicationConfig)
+    {
+        parent::__construct(self::getCommandName());
+        $this->applicationConfig = $applicationConfig;
+    }
+
+    public function configure()
+    {
+        $this
+            ->addArgument('file', InputArgument::REQUIRED, 'Bootstrap file name')
+        ;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (file_exists($this->applicationConfig->getBootstrapDirectory() . $input->getArgument("file"))) {
+            throw new \Exception("Bootstrap file already exists");
+        }
+
+        foreach ($this->applicationConfig->getBootstrapItems() as $bootstrapItem) {
+            if ($bootstrapItem->getFileName() === $input->getArgument("file")) {
+                $this->generateFile($bootstrapItem);
+                $output->writeln(sprintf("<info>%s generated</info>", $bootstrapItem->getFileName()));
+                return;
+            }
+        }
+
+        throw new \Exception (sprintf("Bootstrap file %s does not exist", $input->getArgument("file")));
+    }
+
+    public static function getCommandName()
+    {
+        return "bootstrap:generate";
+    }
+
+    private function generateFile(BootstrapItemInterface $bootstrapItem): void
+    {
+        file_put_contents(
+            $this->applicationConfig->getBootstrapDirectory() . $bootstrapItem->getFileName(),
+            sprintf($this->template, '\\' . get_class($bootstrapItem->getConfigurator()), '$' . $bootstrapItem->getVariableName())
+        );
+    }
+}
---
+++ src/Console/BootstrapListCommand.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace KiwiSuite\Application\Console;
+
+use KiwiSuite\Application\ApplicationConfig;
+use KiwiSuite\ApplicationConsole\Command\CommandInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class BootstrapListCommand extends Command implements CommandInterface
+{
+    /**
+     * @var ApplicationConfig
+     */
+    private $applicationConfig;
+
+    /**
+     * BootstrapListCommand constructor.
+     */
+    public function __construct(ApplicationConfig $applicationConfig)
+    {
+        parent::__construct(self::getCommandName());
+        $this->applicationConfig = $applicationConfig;
+    }
+
+    public function configure()
+    {
+
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $data = [];
+        foreach ($this->applicationConfig->getBootstrapItems() as $bootstrapItem) {
+            $data[] = [
+                $bootstrapItem->getFileName(),
+                (file_exists($this->applicationConfig->getBootstrapDirectory() . $bootstrapItem->getFileName())) ? '<info>Used</info>' : '<comment>Unused</comment>',
+            ];
+        }
+
+        $io->table(
+            ['File', 'Status'],
+            $data
+        );
+    }
+
+    public static function getCommandName()
+    {
+        return "bootstrap:list";
+    }
+}
