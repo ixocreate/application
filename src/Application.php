6c71880ffbd121290a4d0987ce2852e6d28a5489
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-06-12 10:49:00 +0200

    add application version constant

---
+++ src/Application.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOLIT GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Application;
+
+final class Application
+{
+    /**
+     * @var string
+     */
+    const VERSION = '0.3';
+}
